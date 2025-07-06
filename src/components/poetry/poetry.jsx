96c93e81df0f2b99529ae1abea22472c8a5270a0
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-06 21:46:00 +0800

    move poems into json file

--- src/components/poetry/poetry.jsx
+++ src/components/poetry/poetry.jsx
@@ -1,23 +1,13 @@
 import './poetry.scss'
-
-const poetry = [{
-    id: 1,
-    body: "spear-wound moonlight lights their many forms \n former selves being the scars \n red star, rise into the air\nspirits remain\nthey are invisible, black in the darkness\nstaying near the grave, eternal home\nbirds of lifeless flight"
-},
-{id: 2,
-    body: "keeps me company\nwarm body and memory\nyou ask where i would stab you \nand bite my fingers \na 9 lives heart remembers:\ncut to pieces sewn to many\nhere and here - and here"
-},
-{
-id: 3,
-body: "galactic vampire \nto the tick of world rhythm \ndance until clock strike"
-}];
+import poemData from '../../db/poetry.json'
 
 export default function Poetry() {
-    const poetryItems = poetry.map(poem =>
+
+    const poetryItems = poemData.map(poem =>
         <li key={poem.id}>
             {poem.body}
-        </li>
-    )
+        </li>)
+
     return (
         <div>
             <ul className="display-linebreak">{poetryItems}</ul>
