40f47f77faccaae492096a3adb51a7ca8513da1d
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-08 18:41:22 +0800

    improving styling of guestbook

--- src/components/PoetryPage.jsx
+++ src/components/PoetryPage.jsx
@@ -7,7 +7,7 @@ import './PoetryPage.scss'
 function PoetryPage() {
     return (
         <div>
-            <h1><a href="/">Berry me in the realm of release...</a></h1>
+            <h1><a className="title" href="/">Berry me in the realm of release...</a></h1>
             <img className="chi" src={chiStar} width="300" />
             <Poetry></Poetry>
             <Track classname="mobile" trackId="1"></Track>
