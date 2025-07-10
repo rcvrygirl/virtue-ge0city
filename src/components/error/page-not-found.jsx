7933ad62c3693a378b16da1bb06a2f44d014c3d7
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-10 17:08:26 +0800

    add snowfall to error page

--- src/components/error/page-not-found.jsx
+++ src/components/error/page-not-found.jsx
@@ -1,13 +1,22 @@
 import { useRouteError } from "react-router-dom";
 import './page-not-found.scss'
 import maid from '../../assets/maid.png'
+import Snowfall from 'react-snowfall'
+import Header from '../header/header'
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
+    <>
+    <Header />
     <div className="error-page">
+      <Snowfall 
+        snowflakeCount={100}
+        speed={[0.1, 3]}
+        wind={[-0.5, 2]}
+      />
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
@@ -15,5 +24,6 @@ export default function ErrorPage() {
       </p>
       <img src={maid} />
     </div>
+    </>
   );
 }
