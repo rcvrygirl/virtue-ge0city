0d19eb5d065e6289922be7514b7aa68b76278676
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-21 19:02:53 +0800

    tumblr page updates

--- src/components/tumblr/tumblr.jsx
+++ src/components/tumblr/tumblr.jsx
@@ -2,9 +2,9 @@ function Tumblr() {
     return (
       <div>
         <iframe
+        className="tumblr-panel"
           src="https://petracoding.github.io/pinterest/board.html?link=berrryscary/mima-geocity/&hideHeader=1&hideFooter=1&transparent=1"
           width="100%"
-          height="600"
           title="Tumblr Board"
         ></iframe>
       </div>
