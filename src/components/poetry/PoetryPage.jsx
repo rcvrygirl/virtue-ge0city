5889e14cae226cbf6311c2b2dd9f06c5527e806f
Calista Pemberton <calista@Calistas-MacBook-Air.local>
2025-07-22 13:17:32 +0800

    add loader to poetry front page

--- src/components/poetry/PoetryPage.jsx
+++ src/components/poetry/PoetryPage.jsx
@@ -2,10 +2,12 @@ import Poetry from './poetry.jsx'
 import Track from '../audio/tracks.jsx'
 import chiStar from '../../assets/chi-and-a-star.png'
 import './PoetryPage.scss'
+import Loader from '../loader/Loader.jsx'
 
 
 function PoetryPage() {
     return (
+        <Loader>
         <div>
         <div className="p-page">
             <h1><a className="title" href="/">Berry me in the realm of release...</a></h1>
@@ -15,6 +17,7 @@ function PoetryPage() {
             <div className="spacer"></div>
         </div>
         </div>
+        </Loader>
     )
 
 }
